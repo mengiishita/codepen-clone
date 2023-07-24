07ac2d9fd2b31110259d5726fdb94baf93fe2fbb
Ishita Mengi <mengiishita@gmail.com>
2023-07-24 16:40:23 +0200

    Installing material-ui and creating header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="App">Codepen</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
